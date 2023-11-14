df277ccecf234178cd100b841c38e9bacf66f0da
mikega1354 <tabrizimike@gmail.com>
2023-11-14 14:08:12 -0800

    Adds shuffledDeck and renderRandomCard functions

--- js/main.js
+++ js/main.js
@@ -1,39 +1,59 @@
 	/*----- constants -----*/
 	const suits = ['s', 'c', 'd', 'h'];
 	const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
-	const board =[0,0,0,0,0,0,0,0];
-    const boardEl= document.getElementById('c0r1');
+	const originalDeck = buildOriginalDeck();
+  
 	/*----- state variables -----*/
-
+	let shuffleDeck = getNewShuffledDeck();
 
 	/*----- cached elements  -----*/
+	
 
+	
 
 	/*----- event listeners -----*/
-
+	
+	
 
 	/*----- functions -----*/
 	
-	deck = buildOriginalDeck();
-    const firstCard = deck[0]; // Accesses the first card
-    boardEl.innerText = firstCard.face + " with value " + firstCard.value;
 
-  
 	
 	function buildOriginalDeck() {
 		const deck = [];
-		// Use nested forEach to generate card objects
+	
 		suits.forEach(function(suit) {
 		  ranks.forEach(function(rank) {
 			deck.push({
-			  // The 'face' property maps to the library's CSS classes for cards
+			 
 			  face: `${suit}${rank}`,
-			  // Setting the 'value' property for game of blackjack, not war
+			 
 			  value: Number(rank) || (rank === 'A' ? 11 : 10)
 			});
 		  });
 		});
 		return deck;
 	  }
-	 
+	  function renderRandomCardInContainer(shuffleDeck, container) {
+		if (shuffleDeck.length > 0) {
+			
+			const randomIndex = Math.floor(Math.random() * shuffleDeck.length);
+			const randomCard = shuffleDeck[randomIndex];
+			const cardHtml = `<div class="card ${randomCard.face}"></div>`;	         
+			container.innerHTML += cardHtml;
+			shuffleDeck.splice(randomIndex, 1);
+		}
+	}
+
+	function getNewShuffledDeck() {
+		
+		const tempDeck = [...originalDeck];
+		const newShuffledDeck = [];
+		while (tempDeck.length) {		 
+		  const rndIdx = Math.floor(Math.random() * tempDeck.length);	
+		  newShuffledDeck.push(tempDeck.splice(rndIdx, 1)[0]);
+		}
+		return newShuffledDeck;
+	  }
 	  
+	
